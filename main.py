301d2b0d97e6c63993eceaeaf4cd670edfdb3abe
Vinicius Cruz <vini.cruz.roo@gmail.com>
2026-03-03 09:55:11 -0400

    Atualizando main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import os
 from fastapi import FastAPI
 from app.adapters.dtos.dtos import ChatRequestDTO, ChatResponseDTO
 from app.adapters.ai_adapter import GroqAdapter
 from app.adapters.exceptions.error_handler import add_exception_handlers
 
-app = FastAPI(title="FluentBase: O Seu tutor de inglês na palma da sua mão.")
+app = FastAPI(title="FluentBase: AI Orchestrator")
 add_exception_handlers(app)
+
 ai_service = GroqAdapter()
 
+@app.get("/")
+async def root():
+    return {"status": "online"}
 
 @app.post("/chat", response_model=ChatResponseDTO)
 async def chat_endpoint(request: ChatRequestDTO):
-    analysis_entity = await ai_service.process_text(request.message)
-    return analysis_entity
+    return await ai_service.process_text(request.message)
